1e1612acbfbcb255386da4b9ec32699f7c0fbdc7
Liam Martens <hi@liammartens.com>
2023-01-09 09:54:40 -0500

    feat: add reproduction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,16 @@
+import dynamic from 'next/dynamic';
+import ssrPrepass from 'react-ssr-prepass'
+
+const DynamicComponent = dynamic(() => import('../components/Component'));
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return (
+    <DynamicComponent />
+  )
+}
+
+export const getStaticProps = async () => {
+  await ssrPrepass(<Home />);
+  return { props: {} }
 }
